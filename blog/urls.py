bbc00e661f042ea28f9620da87ed5e6e45e3b7ad
ab43razzak <ab43razzak@gmail.com>
2025-02-15 08:07:21 -0800

    Updated blog list and detail pages with consistent image size and 'Read More' links

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import BlogListView, BlogDetailView
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name='blog_list'),
-    path('<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('', BlogListView.as_view(), name='blog-list'),
+    path('<int:pk>/', BlogDetailView.as_view(), name='blog-detail'),
 ]
